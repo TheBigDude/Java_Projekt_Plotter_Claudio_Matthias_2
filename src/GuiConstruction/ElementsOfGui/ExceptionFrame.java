20d31815ff1077eccf6823a13aca32ff4ab40691
MightyML <matthias.lueoend@students.fhnw.ch>
2015-06-07 15:37:43 +0200

    ...

--- src/GuiConstruction/ElementsOfGui/ExceptionFrame.java
+++ src/GuiConstruction/ElementsOfGui/ExceptionFrame.java
@@ -3,22 +3,14 @@ package GuiConstruction.ElementsOfGui;
 import javax.swing.*;
 
 /**
- * Created by Claudio on 06.06.2015.
+ * Created by Matthias on 06.06.2015.
  */
 public class ExceptionFrame {
     JFrame exceptionFrame = new JFrame();
 
     public ExceptionFrame(){
+        JOptionPane.showMessageDialog(exceptionFrame, "Bitte waelen sie eine Datei aus. (Datei, neue Datei)");
 
-        final int FRAME_WIDTH = 400;
-        final int FRAME_HEIGHT = 75;
-        exceptionFrame.setSize(FRAME_WIDTH, FRAME_HEIGHT);
-        JLabel label = new JLabel("Bitte zuerst eine Datei auswählen");
-        JPanel panel = new JPanel();
-        panel.add(label);
-        exceptionFrame.add(panel);
-        exceptionFrame.setLocationRelativeTo(null);
-        exceptionFrame.setVisible(true);
     }
 
     public JFrame getExceptionFrame(){
